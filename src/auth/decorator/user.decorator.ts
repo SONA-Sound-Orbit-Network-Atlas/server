62e6957322923c73a0d1765dfcebddf56e1b50a9
Hangeony <geonhui95@naver.com>
2025-08-25 18:07:01 +0900

    feat: User 데코레이터 병합 및 인증 예외 처리 추가

---
+++ src/auth/decorator/user.decorator.ts
@@ -0,0 +1,24 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+/**
+ * 요청 핸들러에서 현재 인증된 사용자 정보를 쉽게 가져오기 위한 커스텀 데코레이터
+ */
+
+export const User = createParamDecorator(
+  (key: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    // 인증된 사용자가 없으면 예외 발생
+    if (!user) {
+      throw new UnauthorizedException('로그인이 필요합니다.');
+    }
+
+    //key가 있으면 특정 필드만, 없으면 전체 반환
+    return key ? user[key] : user;
+  }
+);
